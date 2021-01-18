4276da3a6717acafee123ddd3611f71ab2444032
arndom <40564798+arndom@users.noreply.github.com>
2021-01-18 13:06:00 +0100

    Update bot.py
    scheduling functionality

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ with open(r'users.txt', 'r') as f:
 print(users)
 
 # Auto login
-insta = InstaDM(username='arndomguy', password='deK5345bJk434./3r4', headless=False)
+insta = InstaDM(username='username', password='password', headless=False)
 
 def scheduler():
     while True:
@@ -20,57 +20,12 @@ def scheduler():
 def msg():
     for user in users:  
         # Send message
-        insta.sendMessage(user=user, message="Pleasure, say hello to the IG bot, gonna disturb you every damn day. What's next is the for every liked feature; a comment about the debt you keep avoiding for all to see. Oh well, you caused this ")
+        insta.sendMessage(user=user, message="hello")
         
-
+### message sent everyday at 01:05
+### 24 HH:MM format
 schedule.every().day.at("01:05").do(msg)
-schedule.every().day.at("01:35").do(msg)
-schedule.every().day.at("02:05").do(msg)
-schedule.every().day.at("02:35").do(msg)
-schedule.every().day.at("03:05").do(msg)
-schedule.every().day.at("03:35").do(msg)
-schedule.every().day.at("04:05").do(msg)
-schedule.every().day.at("04:35").do(msg)
-schedule.every().day.at("05:05").do(msg)
-schedule.every().day.at("05:35").do(msg)
-schedule.every().day.at("06:05").do(msg)
-schedule.every().day.at("06:35").do(msg)
-schedule.every().day.at("07:05").do(msg)
-schedule.every().day.at("07:35").do(msg)
-schedule.every().day.at("08:05").do(msg)
-schedule.every().day.at("08:35").do(msg)
-schedule.every().day.at("09:05").do(msg)
-schedule.every().day.at("09:35").do(msg)
-schedule.every().day.at("10:05").do(msg)
-schedule.every().day.at("10:35").do(msg)
-schedule.every().day.at("11:05").do(msg)
-schedule.every().day.at("11:35").do(msg)
-schedule.every().day.at("12:05").do(msg)
-schedule.every().day.at("12:35").do(msg)
-schedule.every().day.at("13:05").do(msg)
-schedule.every().day.at("13:35").do(msg)
-schedule.every().day.at("14:05").do(msg)
-schedule.every().day.at("14:35").do(msg)
-schedule.every().day.at("15:05").do(msg)
-schedule.every().day.at("15:35").do(msg)
-schedule.every().day.at("16:05").do(msg)
-schedule.every().day.at("16:35").do(msg)
-schedule.every().day.at("17:05").do(msg)
-schedule.every().day.at("17:35").do(msg)
-schedule.every().day.at("18:05").do(msg)
-schedule.every().day.at("18:35").do(msg)
-schedule.every().day.at("19:05").do(msg)
-schedule.every().day.at("19:35").do(msg)
-schedule.every().day.at("20:05").do(msg)
-schedule.every().day.at("20:35").do(msg)
-schedule.every().day.at("21:05").do(msg)
-schedule.every().day.at("21:35").do(msg)
-schedule.every().day.at("22:05").do(msg)
-schedule.every().day.at("22:35").do(msg)
-schedule.every().day.at("23:05").do(msg)
-schedule.every().day.at("23:35").do(msg)
-schedule.every().day.at("00:05").do(msg)
-schedule.every().day.at("00:35").do(msg)
+
 
 
 if __name__ == '__main__':
